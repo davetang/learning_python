ff7c30b8d34b512967cc88b937d01170a44cc150
Dave Tang <davetingpongtang@gmail.com>
2022-08-25 10:02:12 +0900

    Check if container exists

--- run_docker_tensorflow_notebook.sh
+++ run_docker_tensorflow_notebook.sh
@@ -7,28 +7,49 @@ image=jupyter/tensorflow-notebook:${version}
 container_name=tensorflow-notebook
 port=10000
 
-# If you change the notebook username using the following:
-#
-#     -e NB_USER="my-username" \
-#
-# You will not be able to list currently running servers to obtain a token.
-# Therefore just use the username jovyan
-#
-# In addition, you need to run as root (`--user root`)
-# or else you will not be able to edit mounted files
+start_container () {
 
-docker run -d \
-   --rm \
-   -p ${port}:8888 \
-   --name ${container_name} \
-   --user root \
-   -e NB_UID=$(id -u) \
-   -e NB_GID=$(id -g) \
-   -v $(pwd):/home/jovyan/work \
-   ${image}
+   # If you change the notebook username using the following:
+   #
+   #     -e NB_USER="my-username" \
+   #
+   # You will not be able to list currently running servers to obtain a token.
+   # Therefore just use the username jovyan
+   #
+   # In addition, you need to run as root (`--user root`)
+   # or else you will not be able to edit mounted files
 
->&2 echo ${container_name} listening on port ${port}
->&2 echo -e "Run the following to get the token:\ndocker exec ${container_name} jupyter notebook list"
+   docker run -d \
+      --rm \
+      -p ${port}:8888 \
+      --name ${container_name} \
+      --user root \
+      -e NB_UID=$(id -u) \
+      -e NB_GID=$(id -g) \
+      -v $(pwd):/home/jovyan/work \
+      ${image}
+
+   >&2 echo ${container_name} listening on port ${port}
+   >&2 echo -e "Run the following to get the token:\ndocker exec ${container_name} jupyter server list"
+
+}
+
+check_container () {
+
+   docker container inspect ${container_name} > /dev/null 2>&1
+   # $? == 0 if container exists
+   if [[ $? > 0 ]]; then
+      start_container
+   else
+      >&2 echo ${container_name} already exists
+      exit 1
+   fi
+
+}
+
+# the || is necessary for preventing `set -e` immediately exiting
+# if the container already exists
+check_container || true
 
 >&2 echo Done
 exit 0
