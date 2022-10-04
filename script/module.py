d2a1c823f8a418f2de48905792d822404a706d2e
Dave Tang <davetingpongtang@gmail.com>
2022-10-04 15:40:36 +0900

    Importing

--- script/module.py
+++ script/module.py
@@ -7,6 +7,10 @@
 
 import maths
 
+# https://note.nkmk.me/en/python-import-usage/
+# import only one item and as an alias
+from root import newtons_method as sqrt
+
 print("2 + 2 =", maths.two_num(2, 2, '+'))
 
 my_num = 8128
@@ -23,4 +27,6 @@ print('The sum of the divisors of %d is %d' % (my_num, divisor_sum))
 
 # accessing a variable from the maths module
 print("my_seed in the maths module is %d" % maths.my_seed)
+
+print(f"the square root of {my_num} is {sqrt(my_num)}")
 
