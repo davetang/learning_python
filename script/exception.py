d6a8527f69d23df38b73c3cc80e4114c17a89c0d
Dave Tang <davetingpongtang@gmail.com>
2022-11-04 11:01:32 +0900

    Add assert

--- script/exception.py
+++ script/exception.py
@@ -7,11 +7,15 @@
 from inspect import getmro
 
 # all exceptions in Python are instances of a class derived from BaseException
-print(getmro(ValueError),"\n")
+# https://docs.python.org/3/library/inspect.html#inspect.getmro
+# Return a tuple of class cls’s base classes, including cls, in method
+# resolution order.
+print(f"Method resolution order of ValueError: {getmro(ValueError)}\n")
 
 def as_int(x):
     # test code for errors
     try:
+        print(f"Trying int({x})")
         int(x)
     # handle error
     except ValueError:
@@ -21,16 +25,22 @@ def as_int(x):
         print("Another exception was raised")
     # this runs if no errors were raised
     else:
+        print("No errors were raised")
         return(int(x))
     # runs regardless of error or not
     finally:
-        print("try except has finished for input [{}]".format(x))
+        print(f"try/except has finished for input [{x}]")
 
-print("Running as_int(10) returns {}\n".format(as_int(10)))
-print("Running as_int('str') returns {}\n".format(as_int('str')))
+print(f"Running as_int(10) returns [{as_int(10)}]\n")
+print(f"Running as_int('str') returns {as_int('std')}\n")
+
+assert 2 + 2 == 4
+try:
+    assert 2 + 2 == 5
+except:
+    print("Keep going after assertion error\n")
 
 # raise keyword to raise an exception
 my_str = 8128
 if not type(my_str) is str:
     raise TypeError("{} is not a string".format(my_str))
-
