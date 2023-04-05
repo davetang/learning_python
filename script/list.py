28fb0bd92b038645a19c54c10e12442f571bdeaa
Dave Tang <davetingpongtang@gmail.com>
2023-04-05 15:50:24 +0900

    Create list of list using list comprehension

--- script/list.py
+++ script/list.py
@@ -12,7 +12,7 @@ print("print(words) = ", words)
 
 # read as join(' ', words)
 print("print(' '.join(words)) = ", ' '.join(words))
-print("print('#'.join(words))", '#'.join(words))
+print("print('#'.join(words)) = ", '#'.join(words))
 
 # the * operator can be used to unpack an iterable when calling a function
 print("print(*words) = ", *words, "\n")
@@ -38,5 +38,15 @@ print("Slicing with words[3:5] does not include the end index =", words[3:5])
 
 # create new list of words that contain the letter "o"
 has_o = [x for x in words if "o" in x]
-print('these words contain the letter "o":', *has_o)
+print(f'Within the list {words}, the following contain the letter "o":', *has_o)
 
+# create a list of nine zeros
+print([0 for i in range(9)])
+
+# create a list of list
+print("A list of list")
+nrow, ncol = [9, 9]
+m = [[0 for i in range(ncol)] for j in range(nrow)]
+m[4][4] = 5
+for i in m:
+    print(i)
