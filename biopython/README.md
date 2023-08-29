73767eba90b8bd10eeac0d1769b4c052801b6794
Dave Tang <davetingpongtang@gmail.com>
2023-08-29 14:29:33 +0900

    Biopython scripts

---
+++ biopython/README.md
@@ -0,0 +1,26 @@
+# Biopython
+
+Scripts using Biopython.
+
+## Setup
+
+Use `create_venv.sh` to create a virtual environment.
+
+```console
+./create_venv.sh
+```
+
+Then activate it.
+
+```console
+source venv/bin/activate
+
+which python3
+# ~/github/learning_python/biopython/venv/bin/python3
+```
+
+Deactivate once you have finished.
+
+```console
+deactivate
+```
---
+++ biopython/translate.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+#
+# Written by Dave Tang
+# Year 2023
+#
+
+import argparse
+import sys
+import os.path
+from Bio import SeqIO
+ver = "0.1.0"
+parser = argparse.ArgumentParser()
+
+#
+# positional arguments
+#
+
+# default type is string
+parser.add_argument(
+        "fasta",
+        help = "FASTA file",
+)
+
+#
+# optional arguments
+#
+
+# short and long options
+# store True if specified
+parser.add_argument(
+        "-b",
+        "--verbose",
+        help = "verbose mode",
+        action = "store_true"
+)
+parser.add_argument(
+        "-s",
+        "--skip",
+        help = "skip sequences with partial codons",
+        default = False,
+        action = "store_true"
+)
+parser.add_argument(
+        "-v",
+        "--version",
+        help = "show script version and exit",
+        action = "version",
+        version = ver
+)
+
+args = parser.parse_args()
+
+if os.path.exists(args.fasta):
+    for seq in SeqIO.parse(args.fasta, "fasta"):
+        if len(seq) % 3 and args.skip:
+            if args.verbose:
+                print(f"Skipping {seq.description} due to partial codon", file = sys.stderr)
+        else:
+            print(f">{seq.description}\n{seq.translate().seq}")
+else:
+    print(f"{args.fasta} does not exist")
+    quit(1)
