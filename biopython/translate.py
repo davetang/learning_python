95e0c946108feb9114fe5ff18c629e051c095f46
Dave Tang <davetingpongtang@gmail.com>
2023-08-30 14:51:03 +0900

    Stop codon option

--- biopython/translate.py
+++ biopython/translate.py
@@ -41,6 +41,13 @@ parser.add_argument(
         action = "store_true"
 )
 parser.add_argument(
+        "-t",
+        "--stop",
+        help = "Stop translating on stop codon",
+        default = False,
+        action = "store_true"
+)
+parser.add_argument(
         "-v",
         "--version",
         help = "show script version and exit",
@@ -50,13 +57,15 @@ parser.add_argument(
 
 args = parser.parse_args()
 
+print(args.stop)
+
 if os.path.exists(args.fasta):
     for seq in SeqIO.parse(args.fasta, "fasta"):
         if len(seq) % 3 and args.skip:
             if args.verbose:
                 print(f"Skipping {seq.description} due to partial codon", file = sys.stderr)
         else:
-            print(f">{seq.description}\n{seq.translate().seq}")
+            print(f">{seq.description}\n{seq.translate(to_stop=args.stop).seq}")
 else:
     print(f"{args.fasta} does not exist")
     quit(1)
